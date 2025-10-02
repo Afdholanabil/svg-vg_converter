1a52ac9d2f05049cfb85a79ff6338aa6fe2550ae
Afdholanabil <afdholanabil41435@gmail.com>
2025-10-02 08:06:08 +0700

    feat: converter and show vg pages

---
+++ lib/pages/convert_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConvertPages extends StatelessWidget {
+  const ConvertPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/show_vg_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowVgPages extends StatefulWidget {
+  const ShowVgPages({super.key});
+
+  @override
+  State<ShowVgPages> createState() => _ShowVgPagesState();
+}
+
+class _ShowVgPagesState extends State<ShowVgPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
