0ceac49fa2d4ea446db9d6d6dc3322585fc3ce9b
Afdholanabil <afdholanabil41435@gmail.com>
2025-10-03 08:22:45 +0700

    fix conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:svg_vg_converter/pages/show_vg_pages.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,10 +6,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 import 'dart:io';
 import 'dart:typed_data';
-import 'svg_to_vg_converter.dart';
-=======
 import 'compare_page.dart';
->>>>>>> 87a5111af131a0ad6026fa6944d647fac75fff8c
 
 // Tidak ada proses konversi di UI. Konversi hanya lewat CLI:
 // dart run lib/batch_converter.dart
